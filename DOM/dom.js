e93e4bac2bb129b9cc503d29a14303a81976c2de
Amara Stephens <amarastephens@Amaras-MacBook-Pro.local>
2021-09-22 02:09:39 -0500

    first commit

---
+++ DOM/dom.js
@@ -0,0 +1,75 @@
+let btn = document.createElement("button")
+
+btn.addEventListener("click", function () {
+    alert("This button has been clicked!")
+});
+
+let submitButton = document.querySelector("#submitBtn")
+
+submitButton.addEventListener("click", function () {
+    let textValue = document.querySelector("#txt")
+    alert(textValue.value)
+
+});
+
+let div = document.getElementById("div")
+
+div.addEventListener("mouseover", function () {
+    div.style.backgroundColor = "blue"
+
+
+    div.addEventListener("mouseout", function () {
+        div.style.backgroundColor = "white"
+
+
+    })
+})
+
+let paragraph = document.createElement("p");
+document.body.appendChild(paragraph)
+paragraph.textContent = "This is some text for testing purposes"
+
+
+paragraph.addEventListener("click", function () {
+    paragraph.style.color = getRandomColor()
+})
+
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+div2 = document.createElement("div")
+btn2 = document.createElement("button")
+btn2.textContent = "Name Button"
+document.body.appendChild(btn2);
+document.body.appendChild(div2);
+
+btn2.addEventListener("click", function () {
+    span = document.createElement("span");
+    span.textContent = "Amara Stephens"
+    document.body.appendChild(span)
+
+})
+
+let friends = ["Ryan", "Autumn", "Ella", "Meghan", "Zac", "TJ", "Will", "Jenna", "Blake", "Avery"]
+
+friendsButton = document.querySelector("#friend-button")
+
+friendsButton.addEventListener("click", function () {
+  
+   
+
+    for (let i = 0; i < friends.length; i++) {
+        let li = document.createElement("li")
+        let ul = document.querySelector("#ul")
+        ul.appendChild(li)
+        li.textContent = friends[i]
+
+    }
+
+})
---
+++ jQuery/jquery.js
@@ -0,0 +1,54 @@
+$("body").append("<button id=buttonSubmit>Alert Button</Submit>")
+$("#buttonSubmit").click(function () {
+    alert("This button has been clicked!")
+})
+
+$("#target").submit(function () {
+    alert($("#txt").val());
+});
+
+$("#div").on("mouseover", function () {
+
+    $("#div").css("background-color", "blue")
+})
+
+$("#div").on("mouseout", function () {
+
+    $("#div").css("background-color", "white")
+})
+
+$("body").append("<p id=paragraph> This is some text for testing purposes</p>")
+$("#paragraph").click(function () {
+    $("#paragraph").css("color", getRandomColor)
+})
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+$("body").append("<button id=anotherbutton> Name Button </button>")
+$("body").append("<div id=div2></div")
+
+$("#anotherbutton").click(function () {
+    $("body").append("<span> Amara Stephens </span>")
+})
+let friends = ["Ryan", "Autumn", "Ella", "Meghan", "Zac", "TJ", "Will", "Jenna", "Blake", "Avery"]
+
+$("#friend-button").click(function () {
+    $("#ul").append("<li id=list></li>")
+
+    $("#list").each(function(){
+        $(this).text(friends)
+        
+   });
+});
+         
+
+    
+
+
